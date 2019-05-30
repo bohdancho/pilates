3a459270382de2edabad638b3d8c47c03dcb318c
Bohdan Chornocondratenko <bogdan.chornocondratenko@gmail.com>
2019-05-30 19:22:47 +0300

    m

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # pilates
-Pilates company landing page
---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(event) { 
+
+    /*
+        Hamburger Navigation
+    */
+
+    document.getElementById('nav__closebtn').addEventListener('click', closeNav);
+    document.getElementById('navbar-btn').addEventListener('click', openNav);
+    
+    function linkCloseNav() {
+        let link = document.getElementsByClassName('nav__link');
+        for (var i = 0; i < link.length; i++) {
+            // console.log(link[i]);
+            link[i].addEventListener('click', closeNav);
+        };
+    };
+
+    linkCloseNav();
+
+    function openNav() {
+        document.getElementById("nav").style.width = "100%";
+    };
+
+    function closeNav() {
+        document.getElementById("nav").style.width = "0";
+    };
+});
